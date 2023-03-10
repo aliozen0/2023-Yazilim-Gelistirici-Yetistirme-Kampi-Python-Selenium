c5a09af4846c4ef926f6aef20b05f909f1b9caab
Ali Özen <113714644+aliozen0@users.noreply.github.com>
2023-03-10 23:33:13 +0300

    Add files via upload

---
+++ 1.Gun/odev1.py
@@ -0,0 +1,63 @@
+# (2023) Yazılım Geliştirici Yetiştirme Kampı - Python & Selenium 
+# Odev 1 Cevapları :
+
+# 1. soru:
+
+# integer veri tipi = tam sayı değerlerini tutar.
+
+# float veri tipi = ondalık sayı değerlerini tutar.
+
+# complex veri tipi = gerçek ve sanal sayı değerlerini tutar.
+
+# string veri tipi =  merin ifadelerini tutar.
+
+# boolean veri tipi = true ya da false tutar mantıksal ifadadelerde kullanılır.
+
+# liste veri tipi = içinde farklı veri tipleri bulundulabilen indexlenebilir ve içindeki değerleri değiştirilebilen liste yapısında veri tipidir.
+
+# tupple veri tipi = içinde farklı veri tipleri bulundulabilen indexlenebilir ve içindeki değerleri değişmeyen veri tipidir.
+
+# sözlük veri tipi = key ve value olarak tanımlanır key value'daki değişkene ulaşılmasına yarar
+
+
+# 2. soru :
+
+# Sitede öğrenci bilgileri kullanabilir bu yuzden bunlar string ile tutulur.
+# ornek : 
+ogrenciAd = "Ali Özen"
+ogrenciNo = "11111111"
+
+# Sitede kurslarda öğrencinin ilerleme yüzdesi integer ile tutulabilir.
+# ornek :
+ogrenciTamamlamaYuzdesi = 80
+
+# Sitede öğrencin aldığı kurslar ve tammaladığı ödevler liste veri tipi ile tutulabilir.
+# ornek :
+dersListesi = ["java programlama","python ve selenium"]
+odevListesi = ["odev1","ode2","odev3"]
+
+
+# 3.soru :
+
+# Sitede bir çok şey şart blokları ile tutulabilir bunlardan bazıları odevi tamamladı olarak işaretledimi ya da kursu bitirdi mi ya da siteye girerken şifre konrolde
+# örnekler:
+    
+if ogrenciTamamlamaYuzdesi == 100:
+    print("Kursu Başarı ile tamamladınız")
+
+else:
+    print("Kurs bitmedi")
+    
+if butun == "tamamlandı":
+    print("birinci odev tammalandı")
+
+else:
+    print("birinci odev tamamlanmadı lutfen odevi yapiniz")
+    
+if şifre == "1234":
+    print("giris basarili")
+    
+else:
+    print("yanlis sifre girdiniz")
+    
+
